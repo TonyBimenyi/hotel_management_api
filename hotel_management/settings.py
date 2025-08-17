98466f4c155ba8eb5607483278ac49716a29376d
TonyBimenyi <tonyblaisb@gmail.com>
2025-08-17 21:37:44 +0800

    asdasd

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'hotels',
+    'users',
+    'bookings',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +81,15 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 
